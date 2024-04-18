c8e695970381a347fc3aa57eb49e0675cb980cd4
vanispurtree1111 <vani.kn@spurtreetech.com>
2024-04-18 11:38:44 +0530

    frontend-app setup along with link

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import {Link } from 'react-router-dom'
+
+const  Navbar = ()=>{
+    return (
+        <header>
+            <div className = "container">
+<link to="/">
+    <h1>Workout Buddy</h1>
+</link>
+            </div>
+        </header>
+    ) 
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = ()=>{
+    return(
+        <div className= "home"
+        >
+            <h2>HOME</h2>
+        </div>
+    )
+}
+
+export default Home
